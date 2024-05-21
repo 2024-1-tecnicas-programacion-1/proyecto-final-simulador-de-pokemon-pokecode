f77852251edce163e92ea9c3d6321c657c6062b3
user <user@email.com>
2024-05-21 12:14:31 -0500

    constructor y métodos

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,42 @@
 package simulador.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public abstract class Pokemon {
+    protected String nombre;
+    protected int salud;
+    protected int puntoDeAtaque;
+    protected TipoPokemon tipo;
+
+
+    public Pokemon(String nombre, int salud, int puntoDeAtaque, TipoPokemon tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntoDeAtaque = puntoDeAtaque;
+        this.tipo = tipo;
+        
+    }
+    public void atacar(){}
+    public void recbibirDaño(){}
+    public void entrenar (){
+        this.puntoDeAtaque +=5;
+        this.salud +=10;
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public int getPuntoDeAtaque() {
+        return puntoDeAtaque;
+    }
+
+    public TipoPokemon getTipo() {
+        return tipo;
+    }
+
+   
 }
