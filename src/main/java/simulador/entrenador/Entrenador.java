9353fc8d51b8817f14c3264f33113a16041e0202
Kamilo Chacon <kamilochacon7@gmail.com>
2024-05-28 18:44:47 -0500

    Ordene y comente la Clase Entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -5,35 +5,54 @@ import java.util.List;
 import simulador.pokemon.Pokemon;
 
 public class Entrenador {
+    
     private String nombre;
     private List<Pokemon> pokemones;
 
     public Entrenador(String nombre) {
+        
         this.nombre = nombre;
         this.pokemones = new ArrayList<>();
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public List<Pokemon> getPokemones() {
-        return pokemones;
-    }
-
-    public void agregarPokemon(Pokemon pokemon) {
-        pokemones.add(pokemon);
-    }
-    
-     public void entrenarPokemon(Pokemon pokemon) {
         
-    }
+    }//Cierra Entrenador
 
+    
+    public String getNombre() {
+        
+        return nombre;
+        
+    }//Cierra getNombre
+
+    
+    public List<Pokemon> getPokemones() {
+        
+        return pokemones;
+        
+    }//Cierra getPokemones
+
+    
+    public void agregarPokemon(Pokemon pokemon) {
+        
+        pokemones.add(pokemon);
+        
+    }//Cierra agregarPokemon
+    
+    
+    public void entrenarPokemon(Pokemon pokemon) {
+        
+    }//Cierra entrenarPokemon
+
+    
     public void mostrarPokemones() {
+        
         System.out.println("Equipo de Pokémones de " + nombre + ":");
+        
         for (int i = 0; i < pokemones.size(); i++) {
             Pokemon pokemon = pokemones.get(i);
             System.out.println((i + 1) + ". " + pokemon.getNombre() + " (Salud: " + pokemon.getSalud() + ", Ataque: " + pokemon.getPuntosDeAtaque() + ", Tipo: " + pokemon.getTipo() + ")");
-        }
-    }
-}
+            
+        }//Cierra For
+        
+    }//Cierra mostrarPokemones
+    
+}//Cierra Entrenador
